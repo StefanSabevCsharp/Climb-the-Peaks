23207961a75930876c185d84e6fd4c10dfa571b6
Stefan Sabev <stevenbg91@gmail.com>
2023-06-16 22:21:19 +0200

    Initial commit

---
+++ Climb the Peaks/Program.cs
@@ -0,0 +1,64 @@
+﻿int[] foodInts = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+int[] staminaInts = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+Stack<int> food = new Stack<int>(foodInts);
+Queue<int> stamina = new Queue<int>(staminaInts);
+Queue<string> strings = new Queue<string>();
+strings.Enqueue("Vihren");
+strings.Enqueue("Kutelo");
+strings.Enqueue("Banski Suhodol");
+strings.Enqueue("Polezhan");
+strings.Enqueue("Kamenitza");
+Queue<string> result = new Queue<string>();
+for (int i = 0; i < 7; i++)
+{
+    if (food.Count == 0 || stamina.Count == 0)
+    {
+        break;
+    }
+   int currentFood = food.Pop();
+    int currentStamina = stamina.Dequeue();
+    int sum = currentFood + currentStamina;
+    if (sum >= 80 && strings.Contains("Vihren"))
+    {
+        
+        string currentString = strings.Dequeue();
+        result.Enqueue(currentString);
+      
+    }
+    else if (sum >= 90 && strings.Contains("Kutelo") && result.Contains("Vihren"))
+    {
+      
+        string currentString = strings.Dequeue();
+        result.Enqueue(currentString);
+    }
+    else if (sum >= 100 && strings.Contains("Banski Suhodol") &&  result.Contains("Vihren"))
+    {
+       
+        string currentString = strings.Dequeue();
+        result.Enqueue(currentString);
+    }
+    else if (sum >= 60 && strings.Contains("Polezhan") && result.Contains("Banski Suhodol"))
+    {
+        string currentString = strings.Dequeue();
+        result.Enqueue(currentString);
+    }
+    else if(sum >= 70 && strings.Contains("Kamenitza") && result.Contains("Polezhan"))
+    {
+        string currentString = strings.Dequeue();
+        result.Enqueue(currentString);
+    }
+}
+if (result.Count == 5)
+{
+    Console.WriteLine("Alex did it! He climbed all top five Pirin peaks in one week -> @FIVEinAWEEK");
+    Console.WriteLine("Conquered peaks:");
+   
+        Console.WriteLine(string.Join(Environment.NewLine, result));
+
+    
+}
+else
+{
+    Console.WriteLine("Alex failed! He has to organize his journey better next time -> @PIRINWINS");
+    Console.WriteLine(string.Join(Environment.NewLine,result));
+}
---
+++ Navy Battle/Program.cs
@@ -0,0 +1,180 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+char[,] matrix = new char[n, n];
+int playerRow = 0;
+int playerCol = 0;
+int cruisersDestroyed = 0;
+int hittedMines = 0;
+for (int i = 0; i < n; i++)
+{
+    string input = Console.ReadLine();
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i, j] = input[j];
+        if (matrix[i, j] == 'S')
+        {
+            playerRow = i;
+            playerCol = j;
+        }
+    }
+}
+string direction = Console.ReadLine();
+
+while (cruisersDestroyed != 3 || hittedMines != 3)
+{
+    if (direction == "left")
+    {
+        if (CheckMine(playerRow, playerCol - 1, matrix))
+        {
+            hittedMines++;
+            if (hittedMines == 3)
+            {
+                Console.WriteLine($"Mission failed, U-9 disappeared! Last known coordinates [{playerRow}, {playerCol}]!");
+                matrix[playerRow, playerCol] = '-';
+                matrix[playerRow, playerCol - 1] = 'S';
+                PrintMatrix(matrix);
+                Environment.Exit(0);
+            }
+
+        }
+        else if (CheckCruiser(playerRow, playerCol - 1, matrix))
+        {
+            cruisersDestroyed++;
+            if (cruisersDestroyed == 3)
+            {
+                Console.WriteLine("Mission accomplished, U-9 has destroyed all battle cruisers of the enemy!");
+                matrix[playerRow, playerCol] = '-';
+                matrix[playerRow, playerCol - 1] = 'S';
+                PrintMatrix(matrix);
+                Environment.Exit(0);
+            }
+        }
+        matrix[playerRow, playerCol] = '-';
+        matrix[playerRow, playerCol - 1] = 'S';
+        playerCol--;
+
+    }
+    else if (direction == "right")
+    {
+        if (CheckMine(playerRow, playerCol + 1, matrix))
+        {
+            hittedMines++;
+            if (hittedMines == 3)
+            {
+                Console.WriteLine($"Mission failed, U-9 disappeared! Last known coordinates [{playerRow}, {playerCol}]!");
+                matrix[playerRow, playerCol] = '-';
+                matrix[playerRow, playerCol + 1] = 'S';
+                PrintMatrix(matrix);
+                Environment.Exit(0);
+            }
+
+        }
+        else if (CheckCruiser(playerRow, playerCol + 1, matrix))
+        {
+            cruisersDestroyed++;
+            if (cruisersDestroyed == 3)
+            {
+                Console.WriteLine("Mission accomplished, U-9 has destroyed all battle cruisers of the enemy!");
+                matrix[playerRow, playerCol] = '-';
+                matrix[playerRow, playerCol + 1] = 'S';
+                PrintMatrix(matrix);
+                Environment.Exit(0);
+            }
+        }
+        matrix[playerRow, playerCol] = '-';
+        matrix[playerRow, playerCol + 1] = 'S';
+        playerCol++;
+    }
+    else if (direction == "up")
+    {
+        if (CheckMine(playerRow - 1, playerCol, matrix))
+        {
+            hittedMines++;
+            if (hittedMines == 3)
+            {
+                Console.WriteLine($"Mission failed, U-9 disappeared! Last known coordinates [{playerRow}, {playerCol}]!");
+                matrix[playerRow, playerCol] = '-';
+                matrix[playerRow - 1, playerCol] = 'S';
+                PrintMatrix(matrix);
+                Environment.Exit(0);
+            }
+
+        }
+        else if (CheckCruiser(playerRow - 1, playerCol, matrix))
+        {
+            cruisersDestroyed++;
+            if (cruisersDestroyed == 3)
+            {
+                Console.WriteLine("Mission accomplished, U-9 has destroyed all battle cruisers of the enemy!");
+                matrix[playerRow, playerCol] = '-';
+                matrix[playerRow - 1, playerCol] = 'S';
+                PrintMatrix(matrix);
+                Environment.Exit(0);
+            }
+        }
+        matrix[playerRow, playerCol] = '-';
+        matrix[playerRow - 1, playerCol] = 'S';
+        playerRow--;
+    }
+    else if (direction == "down")
+    {
+        if (CheckMine(playerRow + 1, playerCol, matrix))
+        {
+            hittedMines++;
+            if (hittedMines == 3)
+            {
+                Console.WriteLine($"Mission failed, U-9 disappeared! Last known coordinates [{playerRow}, {playerCol}]!");
+                matrix[playerRow, playerCol] = '-';
+                matrix[playerRow + 1, playerCol] = 'S';
+                PrintMatrix(matrix);
+                Environment.Exit(0);
+            }
+
+        }
+        else if (CheckCruiser(playerRow + 1, playerCol, matrix))
+        {
+            cruisersDestroyed++;
+            if (cruisersDestroyed == 3)
+            {
+                Console.WriteLine("Mission accomplished, U-9 has destroyed all battle cruisers of the enemy!");
+                matrix[playerRow, playerCol] = '-';
+                matrix[playerRow + 1, playerCol] = 'S';
+                PrintMatrix(matrix);
+                Environment.Exit(0);
+            }
+        }
+        matrix[playerRow, playerCol] = '-';
+        matrix[playerRow + 1, playerCol] = 'S';
+        playerRow++;
+    }
+    direction = Console.ReadLine();
+}
+
+bool CheckCruiser(int row, int col, char[,] matrix)
+{
+    if (matrix[row, col] == 'C')
+    {
+        return true;
+    }
+    return false;
+}
+
+bool CheckMine(int row, int col, char[,] matrix)
+{
+    if (matrix[row, col] == '*')
+    {
+        return true;
+    }
+    return false;
+}
+void PrintMatrix(char[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
